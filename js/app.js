defe91037b4064f9ae5f88ca9267c91a230064e6
Nilline <74991442+Nilline@users.noreply.github.com>
2023-02-24 00:11:11 +0200

    fix script open menu aside

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@
     "use strict";
     document.addEventListener("click", documentActions);
     function documentActions(e) {
-        if (e.target.closest("#icon-menu") || !e.target.closest("#aside")) document.documentElement.classList.toggle("menu-open");
+        if (e.target.closest("#icon-menu")) document.documentElement.classList.toggle("menu-open"); else if (!e.target.closest("#aside")) document.documentElement.classList.remove("menu-open");
     }
 })();
